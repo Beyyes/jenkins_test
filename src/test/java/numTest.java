bc87b45a0f2dca0d7527f8f18a56a6226d050e6f
CGF <cgf1993@foxmail.com>
2017-08-30 20:57:04 +0800

    init

---
+++ src/test/java/numTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by beyyes on 17/8/30.
+ */
+public class numTest {
+
+
+    @Test
+    public void haha() {
+      for (int i = 1;i < 4;i++) {
+          System.out.println("===========");
+          Assert.assertEquals(i, i);
+      }
+    }
+}
